d9999afc884ae0234bfd5ab4fdf0768b9f06b65a
Doug Potts <pottsdl@gmail.com>
2015-07-17 14:39:15 -0600

    Adding Apache license header.

--- reformat_source.bash
+++ reformat_source.bash
@@ -1,7 +1,25 @@
 #!/bin/bash
-# FILE: "/home/dpotts/proto/search_with_git/reformat_sourc.bash" {{{
-# LAST MODIFICATION: "Mon, 15 Jun 2015 16:19:03 (dpotts)"
+# LAST MODIFICATION: "Fri, 17 Jul 2015 14:28:10 ()"
 # (C) 2015 by Douglas L. Potts, <pottsdl@gmail.com>
+#
+# ==============================================================================
+# ==============================================================================
+# Copyright (c) 2015 Douglas Lee Potts
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+# ==============================================================================
+# ==============================================================================
+#
 # $Id:$ }}}
 
 # Generate the source code list, exclude the unit testing framework cod (Unity)
