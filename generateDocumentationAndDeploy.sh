6475125a94faf3aaec54fcc3fda6f096389daf8c
Doug Potts <doug.potts@sncorp.com>
2017-01-26 19:02:29 -0700

    Debugging doxygen deployment

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -88,14 +88,19 @@ if [ -d "html" ] && [ -f "html/index.html" ]; then
     # stayed the same and will only update the changed files.
     git add --all
 
+    echo "Added all changes."
     # Commit the added files with a title and description containing the Travis CI
     # build number and the GitHub commit reference that issued this build.
     git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
+    echo "Changes committed"
 
     # Force push to the remote gh-pages branch.
     # The ouput is redirected to /dev/null to hide any sensitive credential data
     # that might otherwise be exposed.
-    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+    #git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+    echo "Pushing gh-pages changes..."
+    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}"
+    echo "Push complete"
 else
     echo '' >&2
     echo 'Warning: No documentation (html) files have been found!' >&2
